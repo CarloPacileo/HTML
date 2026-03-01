0d3b2093105b6184f9e6baf753161a5a8531f57f
CarloPacileo <pacileocarlo02@gmail.com>
2026-03-01 20:53:01 +0100

    versione 0.2.5.2

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+let youtube = document.getElementById("youtube");
+let tiktok = document.getElementById("tiktok");
+let facebook = document.getElementById("facebook");
+let instagram = document.getElementById("instagram");
 
+function acceso(span) {
+    let img = span.querySelector('img');
+    if (img) img.style.border = "1px solid red";
+    return true;
+}
+
+function spento(span) {
+    let img = span.querySelector('img');
+    if (img) img.style.border = "1px solid transparent";
+    return true;
+}
