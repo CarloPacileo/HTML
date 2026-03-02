c8f4866a969c4fdc6619b7197ab795b03502f4e0
CarloPacileo <pacileocarlo02@gmail.com>
2026-03-02 18:59:05 +0100

    versione 0.2.5.3

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-let youtube = document.getElementById("youtube");
-let tiktok = document.getElementById("tiktok");
-let facebook = document.getElementById("facebook");
-let instagram = document.getElementById("instagram");
 
 function acceso(span) {
     let img = span.querySelector('img');
